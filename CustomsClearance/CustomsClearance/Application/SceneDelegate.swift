c2a62499858b5c56225402414a155ca1ec03bc8c
Oleksandr Chernets <set1>
2023-03-23 14:29:42 +0200

    added pods

---
+++ CustomsClearance/CustomsClearance/Application/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  CustomsClearance
+//
+//  Created by Alexandr Chernets on 20.03.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
